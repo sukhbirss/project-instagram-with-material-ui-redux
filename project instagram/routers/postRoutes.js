b6daa8306a3a93c51a9754e81124b072e7a2fb5e
sukhbirss <70104881+sukhbirss@users.noreply.github.com>
2020-11-13 16:34:02 +0530

    Update postRoutes.js

--- project instagram/routers/postRoutes.js
+++ project instagram/routers/postRoutes.js
@@ -9,8 +9,8 @@ const router = express.Router();
 
 router.use(authController.protect);
 router.post('/',postController.post);
-router.post('/get',postController.getPosts);
-router.post('/mypost',postController.getMyPosts);
+router.get('/get',postController.getPosts);
+router.get('/mypost',postController.getMyPosts);
 router.patch('/like',postController.likes);
 router.patch('/unlike',postController.unLikes);
 router.delete('/mypost',postController.deleteMyPost);
