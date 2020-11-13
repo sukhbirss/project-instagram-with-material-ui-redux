f4c9e4f58cf377f8cb7c268f0bfcdcad15235541
sukhbirss <70104881+sukhbirss@users.noreply.github.com>
2020-11-13 16:45:18 +0530

    Update userRoutes.js

--- project instagram/routers/userRoutes.js
+++ project instagram/routers/userRoutes.js
@@ -9,11 +9,11 @@ router.post('/signup',authController.signup);
 router.post('/login',authController.login);
 router.post('/forgotpassword',authController.forgotPassword);
 router.post('/resetpassword',authController.resetPassword);
-router.post('/alluser',userController.getAllUser);
+router.get('/alluser',userController.getAllUser);
 
 router.use(authController.protect);
-router.post('/',userController.getUser);
-router.post('/profile/:id',userController.getUserData);
+router.get('/',userController.getUser);
+router.get('/profile/:id',userController.getUserData);
 router.patch('/follow',userController.addFollower);
 router.patch('/unfollow',userController.unfollow);
 router.post('/follower',userController.getFollower);
